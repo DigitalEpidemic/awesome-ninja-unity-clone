6b05ccb1274ab49b0857e3c71162c3eb894ae64e
Jeff Polasz <jeff_polasz@hotmail.com>
2018-03-28 17:20:49 -0400

    Added player attacks

---
+++ Assets/Scripts/Player/Attack/PlayerAttacks.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttacks : MonoBehaviour {
+
+	private Animator anim;
+
+	// Animation states
+	private string ANIMATION_ATTACK = "Attack";
+
+	void Awake () {
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		HandleButtonPresses ();
+	}
+
+	void HandleButtonPresses () {
+		if (Input.GetKeyDown (KeyCode.I)) {
+			anim.SetBool (ANIMATION_ATTACK, true);
+		}
+
+		if (Input.GetKeyUp (KeyCode.I)) {
+			anim.SetBool (ANIMATION_ATTACK, false);
+		}
+	}
+
+} // PlayerAttacks
--- Assets/Scripts/Player/Movement/PlayerMoveKeyboard.cs
+++ Assets/Scripts/Player/Movement/PlayerMoveKeyboard.cs
@@ -16,6 +16,9 @@ public class PlayerMoveKeyboard : MonoBehaviour {
 	private string AXIS_X = "Horizontal";
 	private string AXIS_Y = "Vertical";
 
+	// Animation States
+	private string ANIMATION_RUN = "Run";
+
 	void Awake () {
 		anim = GetComponent<Animator> ();
 		motor.movementDirection = Vector2.zero;
@@ -31,9 +34,9 @@ public class PlayerMoveKeyboard : MonoBehaviour {
 		motor.movementDirection = Input.GetAxis (AXIS_X) * screenMovementRight + Input.GetAxis (AXIS_Y) * screenMovementForward;
 
 		if (Input.GetAxis (AXIS_X) != 0 || Input.GetAxis (AXIS_Y) != 0) {
-			anim.SetBool (AnimationStates.ANIMATION_RUN, true);
+			anim.SetBool (ANIMATION_RUN, true);
 		} else {
-			anim.SetBool (AnimationStates.ANIMATION_RUN, false);
+			anim.SetBool (ANIMATION_RUN, false);
 		}
 
 		if (motor.movementDirection.sqrMagnitude > 1) {

6bd5f26c6972e29ad3380ee1fd035bb07c1d9d03
Jeff Polasz <jeff_polasz@hotmail.com>
2018-03-30 18:28:49 -0400

    Finished SkillDamage script and fixed fireball bug

--- Assets/Scripts/Skill/SkillDamage.cs
+++ Assets/Scripts/Skill/SkillDamage.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class SkillDamage : MonoBehaviour {
 
-	void Start () {
-		
-	}
+	public LayerMask zombieLayer;
+	public float radius;
+	public float damageCount;
+	public GameObject damageEffect;
+
+	private EnemyHealth attackTarget;
+	private bool collided;
 
 	void Update () {
-		
+		Collider[] hits = Physics.OverlapSphere (transform.position, radius, zombieLayer);
+
+		foreach (Collider c in hits) {
+			if (c.isTrigger) {
+				continue;
+			}
+			attackTarget = c.gameObject.GetComponent<EnemyHealth> ();
+			collided = true;
+
+			if (collided) {
+				Instantiate (damageEffect, transform.position, transform.rotation);
+				attackTarget.EnemyTakeDamage (damageCount);
+			}
+		}
 	}
 
 } // SkillDamage

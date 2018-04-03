69dff67013fcd035cf98c24c53604647bd0c5655
Jeff Polasz <jeff_polasz@hotmail.com>
2018-04-02 23:56:12 -0400

    Added player move joystick (Plugin)

---
+++ Assets/Scripts/Player/Movement/PlayerMoveJoystick.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveJoystick : MonoBehaviour {
+
+	private Transform playerTransform;
+	private Animator anim;
+
+	private AudioSource audioSource;
+	public AudioClip footStep1, footStep2;
+
+	private string ANIMATION_RUN = "Run";
+
+	void Awake () {
+		anim = GetComponent<Animator> ();
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.volume = 0.5f;
+		playerTransform = this.transform;
+	}
+
+	void OnEnable () {
+		EasyJoystick.On_JoystickMove += JoystickMove;
+		EasyJoystick.On_JoystickMoveEnd += JoystickMoveEnd;
+	}
+
+	void OnDisable () {
+		EasyJoystick.On_JoystickMove -= JoystickMove;
+		EasyJoystick.On_JoystickMoveEnd -= JoystickMoveEnd;
+	}
+
+	void JoystickMove (MovingJoystick move) {
+		float angle = move.Axis2Angle (true);
+		playerTransform.rotation = Quaternion.Euler (new Vector3 (0, angle - 45, 0));
+		anim.SetBool (ANIMATION_RUN, true);
+	}
+
+	void JoystickMoveEnd (MovingJoystick move) {
+		anim.SetBool (ANIMATION_RUN, false);
+	}
+
+	void FootStepOne (bool play) {
+		if (play) {
+			audioSource.PlayOneShot (footStep1);
+		}
+	}
+
+	void FootStepTwo (bool play) {
+		if (play) {
+			audioSource.PlayOneShot (footStep2);
+		}
+	}
+
+} // PlayerMoveJoystick
